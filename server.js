a07282b2f44bad01c63bf3e5ff33ba536849b791
aseel-alsayeh <aseelalsayeh97@gmail.com>
2021-07-05 19:19:35 +0300

    nth

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ mongoose.connect('mongodb://localhost:27017/user', {
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
-  console.log('Connected');
+  console.log('Connected :)');
 });
 
 userModel.remove({}, ()=>{
