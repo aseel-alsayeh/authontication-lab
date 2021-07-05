62a26585713d7dcd3544dc76a0c92782f00b94bf
aseel-alsayeh <aseelalsayeh97@gmail.com>
2021-07-05 18:41:12 +0300

    finish

--- models/User.model.js
+++ models/User.model.js
@@ -1,31 +1,13 @@
 'use strict';
 
-const mongoose=require('mongoose');
-const booksModel=require('./Books.model')
+const mongoose = require('mongoose');
+const booksModel = require('./Books.model');
 
-/*
-first define a schema 
-schema: description about how the data must looklike in the database
-cats:
-    name:{type:String}
-*/
+const userSchema = new mongoose.Schema({
+  email: { type: String },
+  books: { type: Array },
+});
 
-const userSchema= new mongoose.Schema({
-    email:{type:String},
-    books:{type:Array}
-})
-// const booksdata=new booksModel()
-// booksdata.seedbooks()
+const userModel = mongoose.model('userModel', userSchema);
 
-const userModel=mongoose.model('userModel',userSchema)
-const seeduser=()=>{
-    const userinfo = new userModel({
-        email:'aseelalsayeh97@gmail.com',
-        books:[seedbooks()]
-    })
-console.log(userinfo.books)
-}
-seeduser()
-
-
-module.exports=userModel
+module.exports = userModel;
--- server.js
+++ server.js
@@ -2,53 +2,83 @@
 
 require('dotenv').config();
 const express = require('express');
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const jwksClient = require('jwks-rsa');
 const app = express();
 app.use(cors());
 const PORT = process.env.PORT || 8080;
-const userModel =require('./models/User.model')
-const booksModel =require('./models/Books.model')
+const userModel = require('./models/User.model');
+const booksModel = require('./models/Books.model');
 
-
-
-mongoose.connect('mongodb://localhost:27017/user',
-    { useNewUrlParser: true, useUnifiedTopology: true }
-);
-
-
-const client = jwksClient({
-  jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
+mongoose.connect('mongodb://localhost:27017/user', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-const getKey=(header, callback)=>{
-  client.getSigningKey(header.kid, function(err, key) {
-      const signingKey = key.publicKey || key.rsaPublicKey;
-      callback(null, signingKey);
-    });
-}
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('Connected');
+});
 
-app.get('/authorize',(req,res)=>{
-  const token=req.headers.authorization.split(' ')[1];
-  jwt.verify(token,getKey,{},(err,user)=>{
-      if(err){
-          res.send('invalid token');
-      }
-      res.send(user)
-  })
+const bookone = new booksModel({
+  name: 'how to lie with statistics',
+  description:
+    'How to Lie with Statistics is a book written by Darrell Huff in 1954 presenting an introduction to statistics for the general reader. Not a statistician, Huff was a journalist who wrote many "how to" articles as a freelancer.',
+  status: true,
+});
+const booktwo = new booksModel({
+  name: 'Mein Kampf',
+  description:
+    'Mein Kampf (German: [maɪn ˈkampf]; My Struggle or My Fight) is a 1925 autobiographical manifesto by Nazi Party leader Adolf Hitler.',
+  status: false,
+});
+const bookthree = new booksModel({
+  name: 'Harry Potter',
+  description: `Harry Potter and the Philosophers Stone is a fantasy novel written by British author J. K. Rowling `,
+  status: true,
+});
+
+// bookone.save();
+// booktwo.save();
+// bookthree.save();
+
+const userinfo = new userModel({
+  email: 'aseelalsayeh97@gmail.com',
+  books: [bookone, booktwo, bookthree],
+});
+
+userinfo.save();
+
+const client = jwksClient({
+  jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
+});
+
+const getKey = (header, callback) => {
+  client.getSigningKey(header.kid, function (err, key) {
+    const signingKey = key.publicKey || key.rsaPublicKey;
+    callback(null, signingKey);
+  });
+};
+
+app.get('/authorize', (req, res) => {
+  const token = req.headers.authorization.split(' ')[1];
+  jwt.verify(token, getKey, {}, (err, user) => {
+    if (err) {
+      res.send('invalid token');
+    }
+    res.send(user);
+  });
   res.send(token);
 });
 
-app.get('/test', (request, response) => {
-
-  // TODO: 
-  // STEP 1: get the jwt from the headers
-  // STEP 2. use the jsonwebtoken library to verify that it is a valid jwt
-  // jsonwebtoken dock - https://www.npmjs.com/package/jsonwebtoken
-  // STEP 3: to prove that everything is working correctly, send the opened jwt back to the front-end
-
-})
+app.get('/books', (req, res) => {
+  userModel.find(function (err, user) {
+    if (err) return res.json(err);
+    res.json(user);
+  });
+});
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
